3de2c3f9679f3054a2461ea385c1ce8f027388a0
lawr <lawrance.rsp@gmail.com>
2020-05-13 02:07:09 +0800

    add org-show-notification-handler
    play pomodoro sound

--- init.el
+++ init.el
@@ -717,6 +717,29 @@ before packages are loaded."
     (setq hippie-expand-try-functions-list
           (remove 'yas-hippie-try-expand hippie-expand-try-functions-list))
     (define-key yas-minor-mode-map (kbd "M-/") 'yas-expand))
+
+  ;; (defvar terminal-notifier-command (executable-find "terminal-notifier") "The path to terminal-notifier.")
+  ;; (defun terminal-notifier-notify (title message)
+  ;;   "Show a message with `terminal-notifier-command`."
+  ;;   (start-process "terminal-notifier"
+  ;;                  "*terminal-notifier*"
+  ;;                  terminal-notifier-command
+  ;;                  "-group" "Emacs"
+  ;;                  "-sender" "org.gnu.Emacs"
+  ;;                  "-title" title
+  ;;                  "-message" message
+  ;;                  "-activate" "org.gnu.Emacs")
+  ;;   )
+  ;; (defun timed-notification(time msg)
+  ;;   (interactive "sNotification when (e.g: 2 minutes, 60 seconds, 3 days): \nsMessage: ")
+  ;;   (run-at-time time nil (lambda (msg) (terminal-notifier-notify "Emacs" msg)) msg)) 
+
+  ;; timer 
+  (with-eval-after-load 'org-pomodoro
+    (defun mytimer-notifier-notify (msg)
+      (org-pomodoro-maybe-play-sound :pomodoro))
+    (setq org-show-notification-handler
+          (lambda (msg) (mytimer-notifier-notify msg))))
   )
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.

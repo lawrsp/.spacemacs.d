01dfa7c326b914423bbe5ecfad07be39f6f4e1a6
lawrsp <7957003+lawrsp@users.noreply.github.com>
2023-03-24 03:02:48 +0800

    fix mind-wave layer

--- layers/mind-wave/packages.el
+++ layers/mind-wave/packages.el
@@ -1,3 +1,26 @@
+;;; packages.el --- mind-wave layer packages file for Spacemacs. -*- lexical-binding: t -*-
+;;
+;; Copyright (c) 2012-2022 Sylvain Benner & Contributors
+;;
+;; Author: lawrsp <lawrancersp@gmail.com>
+;; URL: https://github.com/lawrsp
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (setq mind-wave-packages
       '(
         (mind-wave :location (recipe
@@ -38,4 +61,4 @@
         "J" #'mind-wave-chat-ask-with-multiline
         ;; Execute the command mind-wave-chat-ask-insert-line to insert the ----- User ------ separator, continue inputting multiple lines in the buffer, and finally execute mind-wave-chat-ask-send-buffer.
         "l" #'mind-wave-chat-ask-insert-line
-        ))))
+        "s" #'mind-wave-chat-ask-send-buffer))))
